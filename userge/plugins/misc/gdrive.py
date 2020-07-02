48c38587ca830c3588e15100e2fb0557b4a57035
alfiananda88 <65683445+alfiananda88@users.noreply.github.com>
2020-07-02 18:35:07 +0700

    Update gdrive.py

--- userge/plugins/misc/gdrive.py
+++ userge/plugins/misc/gdrive.py
@@ -862,7 +862,7 @@ class Worker(_GDrive):
             await asyncio.sleep(1)
         end_t = datetime.now()
         m_s = (end_t - start_t).seconds
-        index_url = f"{Config.INDEX_PATH_URL}"
+        index_url = f"{Config.INDEX_PATH_URL}/"
         if isinstance(self._output, HttpError):
             out = f"**ERROR** : `{self._output._get_reason()}`"
         elif self._output is not None and not self._is_canceled:
